90fe191800144179e52ee3f1b0899cae9fe40b5c
shmanoj5 <53075530+shmanoj5@users.noreply.github.com>
2019-07-28 23:44:07 +0530

    Update server.R

--- server.R
+++ server.R
@@ -27,7 +27,8 @@ shinyServer(function(input, output) {
   ## annotated df
   annotated_df_calculator <- reactive({
     
-    m_type = udpipe_load_model("D:\\2_p\\Jakki_Personnel\\Studies\\CBA\\T1\\TA\\GroupProject\\TA-Q3\\Q3\\english-ewt-ud-2.4-190531.udpipe")
+    m_type = udpipe_load_model(source_data("https://github.com/shmanoj5/TA-assignment/blob/master/english-ewt-ud-2.4-190531.udpipe?raw=true")
+)
     
     x <- udpipe_annotate(m_type, x = dataset())
     x <- select(as.data.frame(x),-sentence)
